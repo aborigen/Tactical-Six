fd2aff08adcdf0327728490dde33547b6cfcf1f0
Georgy <george.aborin@gmail.com>
2026-04-28 18:03:43 +0000

    remove external fonts, use local only

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
+        body: ['var(--font-inter)', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+        headline: ['var(--font-inter)', 'ui-sans-serif', 'system-ui', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
