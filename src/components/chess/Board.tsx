58a27b6839a290cb2834e121e706b367105ca1f4
Georgy <george.aborin@gmail.com>
2026-09-24 15:11:25 +0000

    In portrait orientation make chess board take all width possible.

--- src/components/chess/Board.tsx
+++ src/components/chess/Board.tsx
@@ -168,7 +168,7 @@ const Board: React.FC<BoardProps> = ({
   return (
     <div 
       onContextMenu={preventContextMenu}
-      className="relative aspect-square w-full portrait:w-auto portrait:h-full portrait:max-h-[52svh] max-w-[550px] sm:max-h-full mx-auto select-none rounded-xl sm:rounded-2xl overflow-hidden shadow-[0_20px_50px_rgba(0,0,0,0.5)] border-4 sm:border-[12px] border-card/80 bg-card ring-1 ring-white/10 group touch-none"
+      className="relative aspect-square w-full portrait:w-full max-w-[550px] mx-auto select-none rounded-xl sm:rounded-2xl overflow-hidden shadow-[0_20px_50px_rgba(0,0,0,0.5)] border-4 sm:border-[12px] border-card/80 bg-card ring-1 ring-white/10 group touch-none"
     >
       <div className="chess-board-grid w-full h-full">
         {game.board.map((rowArr, row) =>
