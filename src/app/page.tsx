381c85cf3da8986c7cdd2a71d7fb14e7d0d82774
Georgy <george.aborin@gmail.com>
2026-09-24 07:46:02 +0000

    Portrait layout has problems on small mobile devices screen. On iPhone 8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -707,7 +707,7 @@ export default function Home() {
           </Card>
         </div>
 
-        <div className="lg:hidden shrink-0 h-[70px] sm:h-[100px] landscape:h-full landscape:w-[280px] px-2 pb-2 sm:pb-4 landscape:p-4 landscape:border-l landscape:border-white/5">
+        <div className="lg:hidden shrink-0 h-[60px] sm:h-[100px] landscape:h-full landscape:w-[280px] px-2 pb-2 sm:pb-4 landscape:p-4 landscape:border-l landscape:border-white/5">
           <div className="h-full bg-card/50 rounded-lg p-1 sm:p-2 border border-white/5 overflow-hidden">
             {EnginePanel}
           </div>
--- src/components/chess/Board.tsx
+++ src/components/chess/Board.tsx
@@ -150,7 +150,7 @@ const Board: React.FC<BoardProps> = ({
   return (
     <div 
       onContextMenu={preventContextMenu}
-      className="relative aspect-square w-full max-w-[550px] max-h-full mx-auto select-none rounded-2xl overflow-hidden shadow-[0_20px_50px_rgba(0,0,0,0.5)] border-[12px] border-card/80 bg-card ring-1 ring-white/10 group touch-none"
+      className="relative aspect-square w-full portrait:w-auto portrait:h-full portrait:max-h-[48svh] max-w-[550px] sm:max-h-full mx-auto select-none rounded-xl sm:rounded-2xl overflow-hidden shadow-[0_20px_50px_rgba(0,0,0,0.5)] border-8 sm:border-[12px] border-card/80 bg-card ring-1 ring-white/10 group touch-none"
     >
       <div className="chess-board-grid w-full h-full">
         {game.board.map((rowArr, row) =>
